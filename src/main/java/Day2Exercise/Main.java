659ed923755b9d76c47cfcc441cc1619116a209c
AgathaFlorence <agathaflorencetjhia@outlook.com>
2024-09-03 15:32:08 +0700

    Push Main Code (No changes yet)

---
+++ src/main/java/Day2Exercise/Main.java
@@ -0,0 +1,10 @@
+package Day2Exercise;
+
+public class Main {
+    static void myMethod() {
+        System.out.println("Hello World!");
+    }
+    public static void main(String[] args){
+        myMethod();
+    }
+}
