a437060c65f02787836daf3f82702bcba0d350e8
AgathaFlorence <agathaflorencetjhia@outlook.com>
2024-09-03 18:02:35 +0700

    Update cm to km

--- src/main/java/Day2Exercise/Main.java
+++ src/main/java/Day2Exercise/Main.java
@@ -10,9 +10,18 @@ public class Main {
         double fahrenheit = input.nextDouble();
 
         double celcius =(( 5 * (fahrenheit - 32.0)) /9.0);
-        System.out.println(fahrenheit + " degree Fahrenheit is equal to " + celcius + " in Celcius");}
+        System.out.println(fahrenheit + " degree Fahrenheit is equal to " + celcius + " in Celcius");
+
+        //Centimeter to Kilometer
+        Scanner inputcm = new Scanner(System.in);
+
+        System.out.print("Input cm: ");
+        double cm = inputcm.nextDouble();
+
+        double km = cm / 100000;
+        System.out.println(cm + " cm equals to " + km + "km");
 
 
-
+    }
 
 }
