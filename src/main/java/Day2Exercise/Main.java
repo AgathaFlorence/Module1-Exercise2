086b0ba86da75f5a18da8282f7c8c2151be98529
AgathaFlorence <agathaflorencetjhia@outlook.com>
2024-09-03 17:44:06 +0700

    Update fahrenheit to celcius

--- src/main/java/Day2Exercise/Main.java
+++ src/main/java/Day2Exercise/Main.java
@@ -1,10 +1,18 @@
 package Day2Exercise;
-
+import java.util.Scanner;
 public class Main {
-    static void myMethod() {
-        System.out.println("Hello World!");
-    }
-    public static void main(String[] args){
-        myMethod();
-    }
+    public static void main(String[] Strings) {
+
+        //Fahrenheit to Celcius
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input a degree in Fahrenheit: ");
+        double fahrenheit = input.nextDouble();
+
+        double celcius =(( 5 * (fahrenheit - 32.0)) /9.0);
+        System.out.println(fahrenheit + " degree Fahrenheit is equal to " + celcius + " in Celcius");}
+
+
+
+
 }
